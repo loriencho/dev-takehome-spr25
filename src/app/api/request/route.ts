2668a44dc84325460faa063f7e67a28e06a03785
Lorien <lorien.yuna.cho@gmail.com>
2025-01-10 16:32:23 -0500

    updated cool page (and added corresponding local assets) and commented code in request route.ts

--- src/app/api/request/route.ts
+++ src/app/api/request/route.ts
@@ -2,15 +2,26 @@ import { ResponseType } from "@/lib/types/apiResponse";
 import { ServerResponseBuilder } from "@/lib/builders/serverResponseBuilder";
 import { InputException } from "@/lib/errors/inputExceptions";
 import { config as dotenvConfig } from "dotenv";
-import { MongoClient, ObjectId} from 'mongodb'
+import { MongoClient, ObjectId } from 'mongodb'
 import { PAGINATION_PAGE_SIZE } from "@/lib/constants/config";
 
 dotenvConfig()
-const uri = (process.env.MONGO_URI as string);
 
+const uri = (process.env.MONGO_URI as string);
 const client = new MongoClient(uri);
 
 export async function GET(request: Request) {
+    /*
+    '/api/request/':
+    Query parameters: page, status
+
+    A GET request that returns all the item requests in the database 
+    in descending order of date created. 
+    The data is paginated by PAGINATED_PAGE_SIZE.
+    If page number is not specified, it defaults to one.
+    Status can also be optionally provided to filter items.
+    */
+
     const databaseName = "crisis-corner";
     const collectionName = "requests";
     const url = new URL(request.url);
@@ -18,10 +29,12 @@ export async function GET(request: Request) {
     const status = url.searchParams.get("status") || "";
     const page_size = PAGINATION_PAGE_SIZE;
 
+    // Connect to database and access collection
     await client.connect();
     const db = client.db(databaseName);
     const collection = db.collection(collectionName);
 
+    // Set item status filter
     const criteria = (
         (['pending', 'completed', 'approved', 'rejected'].includes(status)) ?
             { "status": status }
@@ -29,6 +42,7 @@ export async function GET(request: Request) {
     );
 
     try {
+        // Query collection
         const result = await collection
             .find(criteria).skip((page - 1) * page_size)
             .limit(page_size).sort({ createdDate: -1 }).toArray();
@@ -46,16 +60,33 @@ export async function GET(request: Request) {
 }
 
 export async function PUT(request: Request) {
+    /*
+    '/api/request/':
+    A PUT request that adds a new item request to the database.
+
+    The body should be in the following format:
+        {
+        requestorName: "Jane Doe",
+        itemRequested: "Flashlights"
+        }
+    
+    The creation date and last edited date 
+    are set to the current date and 
+    the status is set to `pending`.
+    */
+
     const databaseName = "crisis-corner";
     const collectionName = "requests";
 
     try {
         const body = await request.json();
 
+        // Connect to database and access collection
         await client.connect();
         const db = client.db(databaseName);
         const collection = db.collection(collectionName);
 
+        // Create item according to schema
         const doc = {
             requestorName: body.requestorName,
             itemRequested: body.itemRequested,
@@ -64,6 +95,7 @@ export async function PUT(request: Request) {
             status: "pending"
         };
 
+        // Query database
         const result = await collection.insertOne(doc);
 
         return new Response(JSON.stringify(result), {
@@ -79,22 +111,42 @@ export async function PUT(request: Request) {
 }
 
 export async function PATCH(request: Request) {
+    /*
+    A PUT request that updates the status 
+    of the item with the given id.
+
+    The body should be in the following format:
+        {
+            id: ________,
+            status: approved
+        }
+
+    The last edited date is set to the current date 
+    and the status is set to the new value.
+    */
+
     const databaseName = "crisis-corner";
     const collectionName = "requests";
+
     try {
         const body = await request.json();
 
+        // Connect to database and access collection
         await client.connect();
         const db = client.db(databaseName);
         const collection = db.collection(collectionName);
 
+        const objectId = ObjectId.createFromHexString(body.id);
+        const updatedFields = {
+            status: body.status,
+            lastEditedDate: new Date()
+        }
+
+        // Query database
         const result = await collection.updateOne(
-            { _id: ObjectId.createFromHexString(body.id)},
+            { _id: objectId },
             {
-                $set: {
-                    status: body.status,
-                    lastEditedDate: new Date()
-                }
+                $set: updatedFields
             },
             { upsert: false }
         );
--- src/app/cool/page.tsx
+++ src/app/cool/page.tsx
@@ -1,19 +1,41 @@
+import Image from "next/image"
+
 export default function Kewl() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-primary text-white gap-5">
-      {/* 
-      Write something unique about you here! 
-      It could be a club you're part of, a weird skill you have, or something special that happened to you.
-      Feel free to put links, images, whatever! 
-      Don't worry about styling- we aren't grading you on this- it's just to get to know you better! :) 
-      */
-      }
-      I really love music, books, and art in general.
-      Here are some drawings I made recently!
-      😎😎😎
-      <img width="240" height="180" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d9ae53df-4601-4537-bf13-9582aa4d37e8/dix9zwy-870f7db1-f841-41d5-a12e-9558a28082c1.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Q5YWU1M2RmLTQ2MDEtNDUzNy1iZjEzLTk1ODJhYTRkMzdlOFwvZGl4OXp3eS04NzBmN2RiMS1mODQxLTQxZDUtYTEyZS05NTU4YTI4MDgyYzEuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0._JJdijDi8z8Qx1uQArfUlcaXUklankuyS3d23e1VKxc"></img>
-      <img width="200" height="300" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d9ae53df-4601-4537-bf13-9582aa4d37e8/dix9zvj-4380e696-c478-427c-a3d9-1e9c239e9333.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Q5YWU1M2RmLTQ2MDEtNDUzNy1iZjEzLTk1ODJhYTRkMzdlOFwvZGl4OXp2ai00MzgwZTY5Ni1jNDc4LTQyN2MtYTNkOS0xZTljMjM5ZTkzMzMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.QM2XjYjfIIAsMyYwvg_qy0u9Ew0pApET_5kP-DLpEHk"></img>
+      <p>
+        I really love music, books (send fantasy recs my way), and art in general!
+        <br></br>
+        I used to do digital art for years, but ever since this year, I picked up an interest in painting.
+        I love nature, and it sneaks into my artwork quite often haha.
+        <br></br>
+        Here are two of my more recent works! I also embedded one of my most listened to spotify playlists last semester.
+        <br></br>
+        I think it has a good mix of all my favorite genres, though it could use a bit more j-pop heh
+        <br></br>
+        alsoo fun fact I used to be in a girl band (we only covered songs) in high school !!
+      </p>
 
+      <div>
+        <Image width="240"
+          height="180"
+          alt="Painting of lilypads in a lake"
+          src="/lake_lilypads.jpg">
+        </Image>
+        <Image width="200"
+          height="300"
+          alt="Digital art of a fish"
+          src="/fish.png">
+        </Image>
+      </div>
+
+      <iframe
+        src="https://open.spotify.com/embed/playlist/6ax3GcDyAjtVc3CNypFx7c?utm_source=generator"
+        width="50%"
+        height="352"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy">
+      </iframe>
     </div>
   );
 }
